b854e3722e9dfd8793de6054a1955df48ebf3411
saad alali <Saad Alali>
2025-05-15 10:34:03 +0300

    Initial commit

--- www/js/cordova-app.js
+++ www/js/cordova-app.js
@@ -1,28 +1,10 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Get the current page filename
-    const currentPage = window.location.pathname.split('/').pop() || 'index.html';
-    
-    // Only keep splash screen for index.html and splash-screen.html
-    if (currentPage !== 'index.html' && currentPage !== 'splash-screen.html') {
-        if (navigator.splashscreen) {
-            navigator.splashscreen.hide();
-            console.log('Hiding splash screen for non-index page: ' + currentPage);
-        }
-    } else {
-        // For index.html, check if we should keep it
-        const keepSplash = sessionStorage.getItem('keep_cordova_splash');
-        if (!keepSplash && navigator.splashscreen) {
-            setTimeout(function() {
-                navigator.splashscreen.hide();
-                console.log('Hiding splash after timeout for index page');
-            }, 500);
-        } else {
-            console.log('Keeping splash screen for index page');
-        }
-    }
-    
+    // Simply initialize the app without any splash screen logic
     console.log('Device is ready');
     localStorage.setItem('app_initialized', 'true');
+    
+    // Let our custom splash screen handle the experience
+    // No need to manipulate any native splash screen
 }
