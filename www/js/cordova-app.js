5fb2c85dcc3d6d6fdc668097081a9ba4b5cbe2a3
saad alali <Saad Alali>
2025-05-15 10:28:52 +0300

    Initial commit

--- www/js/cordova-app.js
+++ www/js/cordova-app.js
@@ -1,10 +1,26 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    const keepCordovaSplash = sessionStorage.getItem('keep_cordova_splash');
+    // Get the current page filename
+    const currentPage = window.location.pathname.split('/').pop() || 'index.html';
     
-    if (!keepCordovaSplash && navigator.splashscreen) {
-        navigator.splashscreen.hide();
+    // Only keep splash screen for index.html and splash-screen.html
+    if (currentPage !== 'index.html' && currentPage !== 'splash-screen.html') {
+        if (navigator.splashscreen) {
+            navigator.splashscreen.hide();
+            console.log('Hiding splash screen for non-index page: ' + currentPage);
+        }
+    } else {
+        // For index.html, check if we should keep it
+        const keepSplash = sessionStorage.getItem('keep_cordova_splash');
+        if (!keepSplash && navigator.splashscreen) {
+            setTimeout(function() {
+                navigator.splashscreen.hide();
+                console.log('Hiding splash after timeout for index page');
+            }, 500);
+        } else {
+            console.log('Keeping splash screen for index page');
+        }
     }
     
     console.log('Device is ready');
